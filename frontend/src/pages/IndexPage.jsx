34469e26dc7beffa6ac0f6b1630289cfc2853c43
Aumkitti <aumkitti.01@gmail.com>
2023-12-13 16:42:28 +0700

    first commit

---
+++ frontend/src/pages/IndexPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from '../components/Post'
+
+function IndexPage() {
+  return (
+    <div>
+      <Post/>
+    </div>
+  )
+}
+
+export default IndexPage
---
+++ frontend/src/pages/PostPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <div className='post-page'>
+        <h1>
+        ถึงแม้เป็นคู่แข่งกันโดยตรงในวงการสตรีมมิ่ง แต่เมื่อรายการไม่ได้ซ้อนทับกัน ดีลธุรกิจก็เกิดขึ้นได้เสมอ ล่าสุด Netflix เซ็นสัญญากับ Disney ซื้อไลเซนส์รายการในเครือ Disney รวม 14 รายการไปฉายบน Netflix ตามกรอบเวลา 18 เดือน
+        </h1>
+        <time>13 December 2023 - 11:26</time>
+        <div className='author'>Aum</div>
+        <div className='image'>
+        <img src="https://www.blognone.com/sites/default/files/externals/b6e92e34a7660b17840c7a959c673eb7.jpg" alt="" />
+        </div>
+        <div className='content'>
+        ทิศทางของธุรกิจสตรีมมิ่งยุคก่อนหน้านี้ แข่งกันที่รายการเอ็กซ์คลูซีฟของแต่ละแพลตฟอร์มอย่างดุเดือด แต่เมื่อช่วงหลังการแข่งขันลดระดับความรุนแรงลง (บาดเจ็บกันทุกฝ่าย) บริษัทสตรีมมิ่งจึงกลับมาขายไลเซนส์รายการให้คู่แข่ง เพื่อหารายได้เสริมทางอื่นบ้าง แบบเดียวกับที่เคยทำกันในวงการสตรีมมิ่งยุคแรกๆ
+        </div>
+    </div>
+  )
+}
+
+export default PostPage
